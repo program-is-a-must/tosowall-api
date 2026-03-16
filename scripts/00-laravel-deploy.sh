13727352938dba4bf151d83f9d253d6577cab0d9
program-is-a-must <balogunmishael7@gmail.com>
2026-03-16 04:54:08 -0700

    Fix SQLite setup for Render

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Creating SQLite database..."
+touch /var/www/html/database/database.sqlite
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
